84c4686c6be9ccb6250a305c45c054b5ad40183e
VVVold <vvvasilevi4@gmail.com>
2017-07-30 02:01:27 +0300

    290717

--- chapter1/src/ru/alhorithms/chapter1/Main.java
+++ chapter1/src/ru/alhorithms/chapter1/Main.java
@@ -1,5 +1,7 @@
 package ru.alhorithms.chapter1;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,14 +9,17 @@ public class Main {
 //        Interval1D interval1D = new Interval1D(6);
 //        CyclicShift cyclicShift = new CyclicShift("1234", "3412");
 //        Mystery mystery = new Mystery("1234");
-//        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
 //        System.out.println(stack.isEmpty());
-//        stack.push(1);
-//        stack.push(2);
-//        stack.push(3);
-//        System.out.println(stack.pop());
-//        System.out.println(stack.pop());
-//        System.out.println(stack.iterator().next());
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(3);
+
+//        System.out.println(stack.reverse(stack.first));
+        System.out.println(stack.reverseRec(stack.first));
+//        System.out.println(stack.reverse1(stack.first));
+
 //        System.out.println(stack.iterator().hasNext());
 //        Queue<Integer> queue = new Queue<>();
 //        System.out.println(queue.isEmpty());
@@ -31,23 +36,23 @@ public class Main {
 //        Dextra2St dextra = new Dextra2St("1+4/7)+3+9)+8*6))))");
 //        Brackets brackets = new Brackets("1 + 4 / 7 ) + 3 + 9 ) + 8 * 6 ) ) ) )");
 
-        ResizingArrayQueueOfString<Integer> Queue = new ResizingArrayQueueOfString<>();
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++) {
-            Queue.enqueue(1);
-        }
-        long spendTime = System.currentTimeMillis() - startTime;
-        System.out.println(spendTime + "ms");
-
-        ResizingArrayQueueOfString<Integer> Queue1 = new ResizingArrayQueueOfString<>();
-        long startTime1 = System.currentTimeMillis();
-        for (int i = 0; i < 5000; i++) {
-            Queue1.enqueue(1);
-            Queue1.enqueue(1);
-        }
-        long spendTime1 = System.currentTimeMillis() - startTime1;
-
-        System.out.println(spendTime + "ms");
-        System.out.println(spendTime1 + "ms");
+//        ResizingArrayQueueOfString<Integer> Queue = new ResizingArrayQueueOfString<>();
+//        long startTime = System.currentTimeMillis();
+//        for (int i = 0; i < 10000; i++) {
+//            Queue.enqueue(1);
+//        }
+//        long spendTime = System.currentTimeMillis() - startTime;
+//        System.out.println(spendTime + "ms");
+//
+//        ResizingArrayQueueOfString<Integer> Queue1 = new ResizingArrayQueueOfString<>();
+//        long startTime1 = System.currentTimeMillis();
+//        for (int i = 0; i < 5000; i++) {
+//            Queue1.enqueue(1);
+//            Queue1.enqueue(1);
+//        }
+//        long spendTime1 = System.currentTimeMillis() - startTime1;
+//
+//        System.out.println(spendTime + "ms");
+//        System.out.println(spendTime1 + "ms");
     }
 }
--- chapter1/src/ru/alhorithms/chapter1/Stack.java
+++ chapter1/src/ru/alhorithms/chapter1/Stack.java
@@ -3,11 +3,12 @@ package ru.alhorithms.chapter1;
 import java.util.Iterator;
 
 public class Stack<Item> implements Iterable<Item> {
-    private int N = 0;
-    private Node first = null;
+    int N = 0;
+    int Ne;
+    Node first = null;
 
 
-    private class Node {
+    class Node {
         Item item;
         Node next;
     }
@@ -29,6 +30,58 @@ public class Stack<Item> implements Iterable<Item> {
         first = first.next;
         N--;
         return item;
+    }
+
+    Item delete(int k) {
+        Node node = first;
+        Item item = null;
+        Item[] items = (Item[])new Object[k];
+        for (int i = 0; i < k; i++) {
+            item = first.item;
+            items[i] = first.item;
+            first = first.next;
+        }
+        for (int i = k - 2; i >= 0; i--) {
+            Node oldfirst = first;
+            first = new Node();
+            first.item = items[i];
+            first.next = oldfirst;
+        }
+        N--;
+        return item;
+    }
+
+//    Item max(Node first) {
+//        first = this.first;
+//        Item max = this.first.item;
+//        if (max < first.item) max = first.item;
+//        if (first ==  null) return max;
+//        return max(first.next);
+//    }
+
+    Node reverse(Node x) {
+        Node first = x;
+        Node reverse = null;
+        while (first != null) {
+            Node second = first.next;
+            first.next = reverse;
+            reverse = first;
+            first = second;
+        }
+        return reverse;
+    }
+
+    Node reverseRec(Node first) {
+        System.out.println(first.item);
+        if(first == null) return null;
+        if (first.next == null) return first;
+        Node second = first.next;
+        Node rest = reverseRec(second);
+        second.next = rest;
+        first.next = null;
+        System.out.println(first.item);
+        return rest;
+
     }
 
     @Override
