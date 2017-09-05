cb147096d8f9d2448b5208119b1e6e1173eeb6bf
VVVold <vvvasilevich@gmail.com>
2017-09-05 17:04:48 +0300

    050917

---
+++ chapter1/src/ru/alhorithms/chapter2/SelectionSort.java
@@ -0,0 +1,42 @@
+package ru.alhorithms.chapter2;
+
+
+public class SelectionSort {
+
+    public static void sort(Comparable a[]) {
+        for (int i = 0; i < a.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (less(a[j], a[min])) min = j;
+                exch(a, min, i);
+            }
+        }
+
+    }
+
+    private static boolean less (Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
+    }
+
+    private static void exch(Comparable a[], int i, int j) {
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void show(Comparable a[]) {
+        for(Comparable s: a) {
+            System.out.print(s + " ");
+        }
+    }
+
+    public static boolean isSorted(Comparable a[]) {
+        for (int i = 1; i < a.length; i++)
+            if (less(a[i], a[i-1])) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
